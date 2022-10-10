aad878fa21889afdebc4e0e63072ded421ec37c6
Weiyi Xia <xwy0220@gmail.com>
2022-10-10 01:13:14 -0500

    created utils.py

--- src/reidrisk/attacker.py
+++ src/reidrisk/attacker.py
@@ -8,22 +8,7 @@ This module contains the functions to creating the probabilistic attacker model 
 import pandas as pd
 import numpy as np
 import csv
-
-def generate_all_binary_string(n):
-    """
-    Given a positive integer number N. The task is to generate all the binary strings of N bits.
-    These binary strings should be in ascending order.
-    :param n:
-    :return an array:
-    """
-    rows_count = 2**n
-    cols_count = n
-    binary_array = np.zeros((rows_count, cols_count))
-    for i in range(rows_count):
-        for j in range(cols_count):
-            if (i//(2**j))%2 == 1:
-                binary_array[i][j] = 1
-    return binary_array
+from utils import generate_all_binary_string
 
 def joined_prob(fields_list, prob_list, all_fields):
     """
---
+++ src/reidrisk/utils.py
@@ -0,0 +1,15 @@
+def generate_all_binary_string(n):
+    """
+    Given a positive integer number N. The task is to generate all the binary strings of N bits.
+    These binary strings should be in ascending order.
+    :param n:
+    :return an array:
+    """
+    rows_count = 2**n
+    cols_count = n
+    binary_array = np.zeros((rows_count, cols_count))
+    for i in range(rows_count):
+        for j in range(cols_count):
+            if (i//(2**j))%2 == 1:
+                binary_array[i][j] = 1
+    return binary_array
